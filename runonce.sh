898f47ca1771aecf3a80178925b40641ef9f0d35
Chris R. Mish <chrism@playingwithsteam.com>
2018-11-30 15:31:40 -0800

    Fixes
    Fixed duplicate command to remove last line from rc.local. Added comments.

--- runonce.sh
+++ runonce.sh
@@ -1,7 +1,4 @@
-sudo sed '$d' /etc/init.d/rc.local
+sudo sed '$d' /etc/rc.local
 sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean; sudo rpi-update
-
-sudo sed '$d' /etc/init.d/rc.local
-
 sudo /root/setup/apps.sh
 sudo reboot
--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 # First: Get ready
 apt-get update; apt-get -y install wget
-
+# Then speed things up
 mkdir /root/setup; cd /root/setup
 sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/config.txt
 sudo cp /boot/config.txt /root/setup/config.txt-old
 sudo rm /boot/config.txt; cp /root/setup/config.txt /boot/
+# Grab the other scripts
 sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/runonce.sh
 sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/apps.sh
-sudo echo "sh /root/setup/runonce.sh" > /etc/init.d/rc.local
+# Set to continue and reboot *ONLY RUN "runonce.sh" ONCE!*
+sudo echo "sh /root/setup/runonce.sh" > /etc/rc.local
 sudo reboot
