901535fd7de93a36036b5f79f2a3965505f5b8d4
Chris R. Mish <chrism@playingwithsteam.com>
2018-11-30 15:21:57 -0800

    Update setup.sh
    File path derps.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # First: Get ready
 apt-get update; apt-get -y install wget
 
-mkdir setup; cd setup
+mkdir /root/setup; cd /root/setup
 sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/config.txt
 sudo cp /boot/config.txt /root/setup/config.txt-old
-sudo rm /boot/config.txt; cp config.txt /boot/
+sudo rm /boot/config.txt; cp /root/setup/config.txt /boot/
 sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/runonce.sh
 sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/apps.sh
 sudo echo "sh /root/setup/runonce.sh" > /etc/init.d/rc.local
