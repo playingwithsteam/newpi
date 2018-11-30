7bdfe9ee6de467433810b8cc89ed4ea9ec941e5a
Chris R. Mish <chrism@playingwithsteam.com>
2018-11-30 15:13:11 -0800

    Update setup.sh
    Fixing to use git URLs for script downloads.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 apt-get update; apt-get -y install wget
 
 mkdir setup; cd setup
-sudo wget http://playingwithsteam.com/newpi/config.txt
+sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/config.txt
 sudo cp /boot/config.txt /root/setup/config.txt-old
 sudo rm /boot/config.txt; cp config.txt /boot/
-sudo wget http://playingwithsteam.com/newpi/runonce.sh
-sudo wget http://playingwithsteam.com/newpi/apps.sh
+sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/runonce.sh
+sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/apps.sh
 sudo echo "sh /root/setup/runonce.sh" > /etc/init.d/rc.local
 sudo reboot
