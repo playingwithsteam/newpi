3d2bc06f046bfa2092c926208ea5db6a543a5675
Chris R. Mish <chrism@playingwithsteam.com>
2018-12-01 02:26:50 -0800

    App added
    Added Calligra because Libre Office has been removed.

--- apps.sh
+++ apps.sh
@@ -1,7 +1,7 @@
 # Install some goodies!
 apt-get update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean; sudo rpi-update
-apt-get -y install filezilla blender gimp inkscape dbmix whois jwhois dnsutils rblcheck gelemental cups-client calibre htop screen
-
+apt-get -y install filezilla blender gimp inkscape dbmix whois jwhois dnsutils rblcheck gelemental cups-client calibre htop screen calligra
+# Double check if the above "calligra" installs Krita and Karbon
 # Update again, maybe it's OCD?
 sudo apt-get -y update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean
 sudo rpi-update
