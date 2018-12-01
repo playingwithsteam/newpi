96d988feeeb0f40fb43e5c098a790b993106520a
Chris R. Mish <chrism@playingwithsteam.com>
2018-11-30 22:03:47 -0800

    Update setup.sh
    Fixed some tired brain problems and not going to bother with setting up to resume after reboot.

--- setup.sh
+++ setup.sh
@@ -2,14 +2,15 @@
 apt-get update; apt-get -y install wget
 # Then speed things up
 mkdir /root/setup; cd /root/setup
-sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/config.txt
-sudo cp /boot/config.txt /root/setup/config.txt-old
-sudo rm /boot/config.txt; cp /root/setup/config.txt
+wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/config.txt
+cp /boot/config.txt /root/config.txt-old
+rm /boot/config.txt; cp /root/setup/config.txt /boot/
 # Grab the other scripts
-sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/runonce.sh
-sudo wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/apps.sh
+wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/runonce.sh
+wget https://raw.githubusercontent.com/playingwithsteam/newpi/master/apps.sh
 #ChMOD - "Because duh"
 chmod 777 /root/setup/*
 # Set to continue and reboot *ONLY RUN "runonce.sh" ONCE!*
-sudo echo "/root/setup/runonce.sh" >> /etc/rc.local
+# Nope, this nonsense is too sloppy:
+#sudo echo "/root/setup/runonce.sh" >> /etc/rc.local
 sudo reboot
