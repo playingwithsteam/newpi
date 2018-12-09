c474d5ff28adef8a66982cbdff35c2e5c0edaa32
Chris R. Mish <chrism@playingwithsteam.com>
2018-12-09 11:32:25 -0800

    Beta - Testing if usable.
    Latest tweaks for newpi (manual version)
    Run setup.sh, runonce.sh and apps.sh.

--- apps.sh
+++ apps.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 # Install some goodies!
-apt-get update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean; sudo rpi-update
-apt-get -y install filezilla blender gimp inkscape dbmix whois jwhois dnsutils rblcheck gelemental cups-client calibre htop screen calligra
-# Double check if the above "calligra" installs Krita and Karbon
-# Update again, maybe it's OCD?
-sudo apt-get -y update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean
-sudo rpi-update
-
+pause -p "[ENTER] to continue: Installing goodies! Find a healthy snack, this will take a few."
+sudo apt-get -y install filezilla blender gimp inkscape dbmix whois jwhois dnsutils rblcheck gelemental cups-client calibre htop screen
+sudo apt-get -y install junior-art junior-doc junior-games-arcade junior-games-card junior-games-puzzle junior-games-sim junior-internet junior-math junior-programming junior-system junior-toys junior-typing junior-writing
+# Installing junior packages + making sure to include "Calligra"
+# (Especially for Krita & Karbon)
 # Woo! start over on a fresh boot!
+pause -p "[ENTER] to continue: Now we'll update, and tidy up followed by a quick, final reboot. Have a lot of fun!"
+sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean
 sudo reboot
--- runonce.sh
+++ runonce.sh
@@ -1,2 +1,5 @@
-apt-get update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean; sudo rpi-update
-sh /root/setup/apps.sh
+#!/bin/sh
+pause -p "[ENTER] to continue: This will take a few, getting fully updated."
+sudo apt-get update; sudo apt-get -y upgrade; sudo apt-get -y autoremove; sudo apt-get clean; sudo rpi-update
+pause -p "[ENTER] to continue: Let's reboot again so that all takes effect."
+sudo reboot
